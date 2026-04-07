ce75e28cbf1b13be45012177da0dfc5de62e93ae
Elton <eltonls@proton.me>
2026-04-07 07:49:14 -0300

    feat: added campaign model and migration

--- shared/api/models/ICampaign.ts
+++ shared/api/models/ICampaign.ts
@@ -1,3 +1,11 @@
+import { IUserDTO } from "./IUser";
+
 export interface Campaign {
+  id: number;
   name: string;
+  description?: string;
+  createdAt: Date;
+  system?: string;
+  ownerId: string;
+  owner: IUserDTO;
 }
